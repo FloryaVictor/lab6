da6d03d5c45aea54f36cac638ad7f402000368d9
FloryaVictor <lapunixa@yandex.ru>
2020-12-12 21:35:22 +0300

    Scripted auto-commit on change (2020-12-12 21:35:22) by gitwatch.sh

--- src/main/java/lab6/ConfActor.java
+++ src/main/java/lab6/ConfActor.java
@@ -2,9 +2,13 @@ package lab6;
 
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+
 public class ConfActor extends AbstractActor {
+    ArrayList<String> servers = new ArrayList<>();
+
     @Override
     public Receive createReceive() {
-        return null;
+
     }
 }
