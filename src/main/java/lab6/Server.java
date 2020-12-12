35d1ff177adb841b254576e0ed6b0d0b5dbfaf69
FloryaVictor <lapunixa@yandex.ru>
2020-12-12 22:21:08 +0300

    Scripted auto-commit on change (2020-12-12 22:21:08) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -1,2 +1,11 @@
-package lab6;public class Server {
+package lab6;
+
+public class Server {
+    public final int port;
+    public Server(int port){
+        this.port = port;
+    }
+    public void start(){
+
+    }
 }
