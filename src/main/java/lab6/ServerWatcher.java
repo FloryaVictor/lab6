75f8ff654d745091484908e4582b0b2d1794b36d
FloryaVictor <lapunixa@yandex.ru>
2020-12-12 21:48:34 +0300

    Scripted auto-commit on change (2020-12-12 21:48:34) by gitwatch.sh

--- src/main/java/lab6/ServerWatcher.java
+++ src/main/java/lab6/ServerWatcher.java
@@ -1,2 +1,15 @@
-package lab6;public class ServerWatcher {
+package lab6;
+
+import akka.actor.Actor;
+import akka.actor.ActorRef;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class ServerWatcher implements Watcher {
+    private final ActorRef confActor;
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
