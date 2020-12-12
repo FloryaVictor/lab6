3ca21490218dc0870d83e12d87ad80d152fbed89
FloryaVictor <lapunixa@yandex.ru>
2020-12-12 21:00:24 +0300

    Scripted auto-commit on change (2020-12-12 21:00:24) by gitwatch.sh

--- src/main/java/lab6/Main.java
+++ src/main/java/lab6/Main.java
@@ -3,6 +3,16 @@ package lab6;
 public class Main {
     public static void main(String[] argv)
     {
-        
+        ZooK)eeper zoo = new ZooK)eeper("1MB27.0.0.1MB:21MB81MB", 3000, this);
+        zoo.create("/servers/s",
+                “data”.getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
+                CreateMode.EPHEMERAL_SEQUENTIAL
+);
+        List<String> servers = zoo.getChildren("/servers", this);
+        for (String s : servers) {
+            byte[] data = zoo.getData("/servers/" + s, false, null);
+            System.out.println("server " + s + " data=" + new String(data));
+        }
     }
 }
