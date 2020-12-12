e4b8eb77a7f68b23ac9a3f5dbbfa3969ad0a9efe
FloryaVictor <lapunixa@yandex.ru>
2020-12-12 21:02:13 +0300

    Scripted auto-commit on change (2020-12-12 21:02:13) by gitwatch.sh

--- src/main/java/lab6/Main.java
+++ src/main/java/lab6/Main.java
@@ -1,18 +1,14 @@
 package lab6;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.List;
+
 public class Main {
-    public static void main(String[] argv)
-    {
-        ZooKeeper zoo = new ZooKeeper("1MB27.0.0.1MB:21MB81MB", 3000, this);
-        zoo.create("/servers/s",
-                data”.getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE ,
-                CreateMode.EPHEMERAL_SEQUENTIAL
-);
-        List<String> servers = zoo.getChildren("/servers", this);
-        for (String s : servers) {
-            byte[] data = zoo.getData("/servers/" + s, false, null);
-            System.out.println("server " + s + " data=" + new String(data));
-        }
+    public static void main(String[] argv)  {
+
     }
 }
