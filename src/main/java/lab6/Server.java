2f0e6c807d9fb2c5ef5756a3ba81a0890ab02385
FloryaVictor <lapunixa@yandex.ru>
2020-12-13 02:49:50 +0300

    Scripted auto-commit on change (2020-12-13 02:49:50) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -1,36 +1,27 @@
 package lab6;
 
 import akka.NotUsed;
-import akka.actor.Actor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
-import akka.http.javadsl.model.Query;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-import lab6.Messages.GetServer;
-import lab6.Messages.RefreshList;
+import static akka.http.javadsl.server.Directives.*;
 import org.apache.zookeeper.*;
-import org.apache.zookeeper.data.ACL;
-import scala.concurrent.Future;
-
 import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ExecutionException;
 
-import static akka.http.javadsl.server.Directives.*;
-import static akka.pattern.Patterns.ask;
+import lab6.Messages.GetServer;
+import lab6.Messages.RefreshList;
 
 public class Server {
     public static final String URL = "url";
@@ -66,6 +57,16 @@ public class Server {
                 });
     }
 
+    public static void initZooKeeper() throws IOException, KeeperException, InterruptedException {
+        keeper = new ZooKeeper(zookeeperConnectString,
+                (int)timeout.getSeconds() * 1000, watcher);
+        keeper.create("/servers/" + PORT, (PORT+"").getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        WatchedEvent e = new WatchedEvent(Watcher.Event.EventType.NodeCreated,
+                Watcher.Event.KeeperState.SyncConnected, "");
+        watcher.process(e);
+    }
+
     public static Watcher watcher = watchedEvent -> {
         if (watchedEvent.getType() == Watcher.Event.EventType.NodeCreated ||
                 watchedEvent.getType() == Watcher.Event.EventType.NodeDeleted ||
@@ -82,18 +83,7 @@ public class Server {
             }
         }
     };
-
-    public static void initZooKeeper() throws IOException, KeeperException, InterruptedException {
-        keeper = new ZooKeeper(zookeeperConnectString,
-                (int)timeout.getSeconds() * 1000, watcher);
-        keeper.create("/servers/" + PORT, (PORT+"").getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-        WatchedEvent e = new WatchedEvent(Watcher.Event.EventType.NodeCreated,
-                Watcher.Event.KeeperState.SyncConnected, "");
-        watcher.process(e);
-    }
-
-
+    
     private static CompletionStage<HttpResponse> fetch(String url) {
         return http.singleRequest(HttpRequest.create(url));
     }
