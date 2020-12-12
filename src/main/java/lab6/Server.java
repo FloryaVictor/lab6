93e60167a1872e5baf4ce40174261a65ddb5d901
FloryaVictor <lapunixa@yandex.ru>
2020-12-13 02:05:41 +0300

    Scripted auto-commit on change (2020-12-13 02:05:41) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -74,7 +74,7 @@ public class Server {
                 watchedEvent.getType() == Watcher.Event.EventType.NodeDataChanged){
             ArrayList<String> newServers;
             for(String s: keeper.getChildren("/servers", this)){
-                byte[] port = keeper.getData("/servers")
+                byte[] port = keeper.getData("/servers/" + s, )
             }
         }
     };
