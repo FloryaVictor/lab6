f3836da59ed627ca37dc526347fb7ad12fe48ab0
FloryaVictor <lapunixa@yandex.ru>
2020-12-12 22:23:15 +0300

    Scripted auto-commit on change (2020-12-12 22:23:15) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -1,9 +1,13 @@
 package lab6;
 
+import akka.stream.ActorMaterializer;
+
 public class Server {
     public final int port;
-    public Server(int port){
+    public final ActorMaterializer mat;
+    public Server(int port, ActorMaterializer mat){
         this.port = port;
+        this.mat = mat;
     }
     public void start(){
 
