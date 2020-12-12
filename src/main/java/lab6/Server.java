03d511f33dab6ddf4f98733ba3c62c691d7d5cce
FloryaVictor <lapunixa@yandex.ru>
2020-12-12 22:30:42 +0300

    Scripted auto-commit on change (2020-12-12 22:30:42) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -1,31 +1,45 @@
 package lab6;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
 public class Server {
-    public final int port;
+    public static final String URL = "url"
 
+    public final int port;
+    public final String host;
     public final ActorSystem system;
     public final ActorMaterializer mat;
     public Server(int port, ActorSystem system, ActorMaterializer mat){
         this.port = port;
         this.system = system;
         this.mat = mat;
+        this.host = "localhost";
     }
 
-
+    public static Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer mat){
+        return Flow.of(HttpRequest.class)
+                .map(req->{
+                    Query q = req.getUri().query();
+                    String url = q.get()
+                })
+    }
     public void start(){
         final Http http = Http.get(system);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
-                ConnectHttp.toHost(, port),
+                ConnectHttp.toHost(host, port),
                 mat
         );
         System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
@@ -33,12 +47,6 @@ public class Server {
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound ->{
-                    system.terminate();
-                    try {
-                        asyncHttpClient.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
                 });
     }
 }
