207da755a00a622839c9a425cf20aa50d4bb2741
FloryaVictor <lapunixa@yandex.ru>
2020-12-13 01:44:14 +0300

    Scripted auto-commit on change (2020-12-13 01:44:14) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -39,26 +39,25 @@ public class Server {
     public static ActorRef confActor;
 
     public static void main(String[] argv) throws IOException {
-//        ZooKeeper keeper = new ZooKeeper(HOST + ":" + PORT, )
-            System.out.println("1" + 2);
-//        PORT = Integer.parseInt(argv[0]);
-//        ActorSystem system = ActorSystem.create("routes");
-//        http = Http.get(system);
-//        confActor = system.actorOf(Props.create(ConfActor.class));
-//        final ActorMaterializer materializer = ActorMaterializer.create(system);
-//        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-//                createRoute().flow(system, materializer);
-//        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-//                routeFlow,
-//                ConnectHttp.toHost(HOST, PORT),
-//                materializer
-//        );
-//        System.in.read();
-//        binding
-//                .thenCompose(ServerBinding::unbind)
-//                .thenAccept(unbound ->{
-//                    system.terminate();
-//                });
+        ZooKeeper keeper = new ZooKeeper(HOST + ":" + PORT, )
+        PORT = Integer.parseInt(argv[0]);
+        ActorSystem system = ActorSystem.create("routes");
+        http = Http.get(system);
+        confActor = system.actorOf(Props.create(ConfActor.class));
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                createRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound ->{
+                    system.terminate();
+                });
     }
 
     private static CompletionStage<HttpResponse> fetch(String url) {
