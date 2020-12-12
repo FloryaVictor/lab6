3ec9b98c942aa3e441677620148f7fde5015374f
FloryaVictor <lapunixa@yandex.ru>
2020-12-12 23:38:57 +0300

    Scripted auto-commit on change (2020-12-12 23:38:57) by gitwatch.sh

--- src/main/java/lab6/Main.java
+++ src/main/java/lab6/Main.java
@@ -14,13 +14,12 @@ import java.util.List;
 public class Main {
     public static void main(String[] argv) throws IOException {
         ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         Server s = new Server(8081, system, materializer, null);
-        ActorSystem system1 = ActorSystem.create("routes");
-        final ActorMaterializer materializer1 = ActorMaterializer.create(system);
-        Server s1 = new Server(8082, system1, materializer1, null);
-        s.start();
+        Server s1 = new Server(8082, system, materializer, null);
         s1.start();
+        s.start();
     }
 
 }
