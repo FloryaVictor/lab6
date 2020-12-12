6d760a9acb4aadac394c7b8b96c3668757dfcb51
FloryaVictor <lapunixa@yandex.ru>
2020-12-12 22:27:57 +0300

    Scripted auto-commit on change (2020-12-12 22:27:57) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -1,15 +1,44 @@
 package lab6;
 
+import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.stream.ActorMaterializer;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class Server {
     public final int port;
+
+    public final ActorSystem system;
     public final ActorMaterializer mat;
-    public Server(int port, ActorMaterializer mat){
+    public Server(int port, ActorSystem system, ActorMaterializer mat){
         this.port = port;
+        this.system = system;
         this.mat = mat;
     }
-    public void start(){
 
+
+    public void start(){
+        final Http http = Http.get(system);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(, port),
+                mat
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound ->{
+                    system.terminate();
+                    try {
+                        asyncHttpClient.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
