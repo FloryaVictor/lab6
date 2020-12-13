92e83469e6f127b8ae63c5452397e9edb26e5b0d
FloryaVictor <lapunixa@yandex.ru>
2020-12-13 03:07:07 +0300

    Scripted auto-commit on change (2020-12-13 03:07:07) by gitwatch.sh

--- src/main/java/lab6/Main.java
+++ src/main/java/lab6/Main.java
@@ -23,7 +23,7 @@ import java.util.concurrent.CompletionStage;
 import lab6.Messages.GetServer;
 import lab6.Messages.RefreshList;
 
-public class Server {
+public class Main {
     public static final String URL = "url";
     public static final String COUNT = "count";
     private static final Duration timeout = Duration.ofSeconds(5);
@@ -56,7 +56,7 @@ public class Server {
                     system.terminate();
                 });
     }
-
+dasds
     public static void initZooKeeper() throws IOException, KeeperException, InterruptedException {
         keeper = new ZooKeeper(zookeeperConnectString,
                 (int)timeout.getSeconds() * 1000, watcher);
